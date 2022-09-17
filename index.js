47905bccbbd9f2ed0b3ca7dcf06d3a6e7e63e933
ayeshaqb <ayesha.bajwa@gmail.com>
2022-09-16 19:19:56 -0500

    "init function"

--- index.js
+++ index.js
@@ -63,7 +63,9 @@ const questions = [
 function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+  inquirer.prompt(questions).then(writeToFile());
+}
 
 // Function call to initialize app
 init();
